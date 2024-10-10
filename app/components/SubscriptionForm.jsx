6ffe180e5dace1700a12bac749c92a75c4b9b809
Your Name <ekka.beaula@gmail.com>
2024-10-10 13:13:59 +0200

    form done

--- app/components/SubscriptionForm.jsx
+++ app/components/SubscriptionForm.jsx
@@ -1,10 +1,26 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
-  userName: z.string().min(3).max(50),
-  email: z.string().email(),
+  fullName: z
+    .string()
+    .min(3, { message: "Full Name must be more than 3 characters" })
+    .max(50, { message: "Full name must not be more that 50 characters" }),
+  email: z.string().email({
+    message: "Invalid email",
+  }),
 });
 export default function SubscriptionForm() {
   const form = useForm({
@@ -14,12 +30,45 @@ export default function SubscriptionForm() {
       email: "",
     },
   });
+
+  function onSubmit(values) {
+    console.log("value", values);
+  }
+
   return (
-    <div>
-      This is subscribtion form
-      <div>
-        <Button>Submit</Button>
-      </div>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4 p-14 rounded-md shadow-md w-[400px]">
+        {/**full Name */}
+        <FormField
+          control={form.control}
+          name="fullName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Full Name</FormLabel>
+              <FormControl>
+                <Input placeholder="John Doe" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/**Email */}
+        <FormField
+          name="email"
+          contorl={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="john.doe@example.com" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <div>
+          <Button className="mt-8">Submit</Button>
+        </div>
+      </form>
+    </Form>
   );
 }
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import SubscriptionForm from "./components/SubscriptionForm";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex items-center justify-center w-full min-h-screen mx-auto mt-10 ">
       <SubscriptionForm />
       <Subscribers />
     </div>
