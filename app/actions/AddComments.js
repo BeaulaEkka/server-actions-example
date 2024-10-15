52b09d02874ac9f35e0e212e41bc6d76db9c14a8
Your Name <ekka.beaula@gmail.com>
2024-10-15 13:20:21 +0200

    comments section added

---
+++ app/actions/AddComments.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default async function AddComments(comment, postId) {
+  console.log("adding comment");
+  console.log(comment, postId);
+  //make a db call
+  //store the commnet
+  return <div></div>;
+}
